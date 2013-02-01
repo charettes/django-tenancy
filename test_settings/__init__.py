ac546043f9fd176a582d7e520aba918b5f35fae9
Simon Charette <charette.s@gmail.com>
2013-02-01 02:54:59 -0500

    Moved sqlite3 specific test settings to its own module

--- test_settings/__init__.py
+++ test_settings/__init__.py
@@ -1,11 +1,5 @@
 SECRET_KEY = 'not-anymore'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-    }
-}
-
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'tenant',
---
+++ test_settings/sqlite3.py
@@ -0,0 +1,8 @@
+from . import INSTALLED_APPS, SECRET_KEY
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
