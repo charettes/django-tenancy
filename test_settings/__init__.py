8427a7732333e1655da7220ead20df4c8e969233
Simon Charette <charette.s@gmail.com>
2013-02-01 03:04:19 -0500

    Added a travis config file

--- test_settings/__init__.py
+++ test_settings/__init__.py
@@ -4,3 +4,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'tenant',
 ]
+
+try:
+    import django_coverage
+except ImportError:
+    pass
+else:
+    INSTALLED_APPS.append('django_coverage')
