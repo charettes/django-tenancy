65b3e3f8e17f103797f8ad00fe1d5562d35a1088
Simon Charette <charette.s@gmail.com>
2013-02-01 04:30:49 -0500

    Renamed app django-tenancy

--- test_settings/__init__.py
+++ test_settings/__init__.py
@@ -2,7 +2,7 @@ SECRET_KEY = 'not-anymore'
 
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
-    'tenant',
+    'tenancy',
 ]
 
 try:
--- test_settings/postgresql_psycopg2.py
+++ test_settings/postgresql_psycopg2.py
@@ -4,7 +4,7 @@ from . import INSTALLED_APPS, SECRET_KEY
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tenant',
+        'NAME': 'tenancy',
         'USER': 'postgres'
     }
 }
