ec3169844eada839c88c3aa5c2b9e6796d121180
Simon Charette <charette.s@gmail.com>
2013-04-02 16:12:11 -0400

    Marked the current version as 0.0.2.dev

--- tenancy/__init__.py
+++ tenancy/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.core.exceptions import ImproperlyConfigured
 
 
-__version__ = (0, 0, 1)
+__version__ = (0, 0, 2, 'dev')
 
 
 def get_tenant_model(origin=None):
