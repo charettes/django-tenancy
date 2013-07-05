b8dfe8e0e3dc0ce88a1073a3d6b9b2f374bdfbdb
Simon Charette <charette.s@gmail.com>
2013-07-05 02:48:37 -0400

    Added coveralls support

--- test_settings/__init__.py
+++ test_settings/__init__.py
@@ -7,18 +7,6 @@ INSTALLED_APPS = [
 ]
 
 try:
-    import django_coverage
-except ImportError:
-    pass
-else:
-    INSTALLED_APPS.append('django_coverage')
-    COVERAGE_MODULE_EXCLUDES = [
-        'tests$',
-        'settings$',
-        'django'
-    ]
-
-try:
     import mutant
 except ImportError:
     pass
