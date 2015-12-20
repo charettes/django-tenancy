010c55cf2720ef8c618094644ed35838422a9505
Simon Charette <charette.s@gmail.com>
2015-12-20 16:33:16 -0500

    Moved comptiblity shims to a dedicated module.

---
+++ tenancy/compat.py
@@ -0,0 +1,70 @@
+from __future__ import unicode_literals
+
+from operator import attrgetter
+
+import django
+
+get_related_model = attrgetter('related_model' if django.VERSION >= (1, 8) else 'model')
+
+
+if django.VERSION >= (1, 8):
+    def get_remote_field_accessor_name(field):
+        return get_remote_field(field).get_accessor_name()
+
+    def get_reverse_fields(opts):
+        return opts._get_fields(forward=False, reverse=True, include_hidden=True)
+
+    def clear_opts_related_cache(model_class):
+        opts = model_class._meta
+        if not opts.apps.ready:
+            return
+        children = [
+            related_object.related_model
+            for related_object in opts.related_objects if related_object.parent_link
+        ]
+        opts._expire_cache()
+        for child in children:
+            clear_opts_related_cache(child)
+else:
+    def get_remote_field_accessor_name(field):
+        return field.related.get_accessor_name()
+
+    def get_reverse_fields(opts):
+        return opts.get_all_related_objects(include_hidden=True)
+
+    _opts_related_cache_attrs = [
+        '_related_objects_cache',
+        '_related_objects_proxy_cache',
+        '_related_many_to_many_cache',
+        '_name_map',
+    ]
+
+    def clear_opts_related_cache(model_class):
+        """
+        Clear the specified model and its children opts related cache.
+        """
+        opts = model_class._meta
+        if not opts.apps.ready:
+            return
+        children = [
+            related_object.model
+            for related_object in opts.get_all_related_objects()
+            if related_object.field.rel.parent_link
+        ]
+        for attr in _opts_related_cache_attrs:
+            try:
+                delattr(opts, attr)
+            except AttributeError:
+                pass
+        for child in children:
+            clear_opts_related_cache(child)
+
+get_remote_field = attrgetter('remote_field' if django.VERSION >= (1, 9) else 'rel')
+
+
+if django.VERSION >= (1, 9):
+    def get_remote_field_model(field):
+        return field.remote_field.model
+else:
+    def get_remote_field_model(field):
+        return field.rel.to
--- tenancy/utils.py
+++ tenancy/utils.py
@@ -3,11 +3,13 @@ from __future__ import unicode_literals
 from contextlib import contextmanager
 from itertools import chain
 
-import django
 from django.apps import apps
-from django.core.exceptions import ImproperlyConfigured
 from django.db import models
-from django.db.models.base import ModelBase
+
+from .compat import (
+    clear_opts_related_cache, get_remote_field, get_remote_field_accessor_name,
+    get_remote_field_model,
+)
 
 
 def get_model(app_label, model_name, **kwargs):
@@ -18,7 +20,7 @@ def get_model(app_label, model_name, **kwargs):
 
 
 @contextmanager
-def _apps_lock():
+def apps_lock():
     # The registry lock is not re-entrant so we must avoid acquiring it
     # during the initialization phase in order to prevent deadlocks.
     if apps.ready:
@@ -31,25 +33,16 @@ def _apps_lock():
 def remove_from_app_cache(model_class, quiet=False):
     opts = model_class._meta
     apps = opts.apps
-    with _apps_lock():
+    app_label, model_name = opts.app_label, opts.model_name
+    with apps_lock():
         try:
-            app_config = apps.get_app_config(opts.app_label)
-        except ImproperlyConfigured:
-            if quiet:
-                return
-            else:
-                raise ValueError(
-                    "No cached models for app %s" % opts.app_label
-                )
-        try:
-            model = app_config.models.pop(opts.model_name)
-        except LookupError:
-            if quiet:
-                return
-            else:
+            model_class = apps.app_configs[app_label].models.pop(model_name)
+        except KeyError:
+            if not quiet:
                 raise ValueError("%r is not cached" % model_class)
         apps.clear_cache()
-        unreference_model(model)
+        unreference_model(model_class)
+    return model_class
 
 
 def get_foward_fields(opts):
@@ -60,24 +53,21 @@ def get_foward_fields(opts):
 
 
 def unreference_model(model):
-    opts = model._meta
-    if not opts.apps.ready:
-        return
     disconnect_signals(model)
-    for field in get_foward_fields(opts):
-        rel = field.rel
-        if field.model is model and rel:
-            to = rel.to
-            if isinstance(to, ModelBase):
-                clear_opts_related_cache(to)
-                rel_is_hidden = rel.is_hidden()
+    for field in get_foward_fields(model._meta):
+        remote_field = get_remote_field(field)
+        if field.model is model and remote_field:
+            remote_field_model = get_remote_field_model(field)
+            if isinstance(remote_field_model, models.base.ModelBase):
+                clear_opts_related_cache(remote_field_model)
+                rel_is_hidden = remote_field.is_hidden()
                 # An accessor is added to related classes if they are not
                 # hidden. However o2o fields *always* add an accessor
                 # even if the relationship is hidden.
                 o2o = isinstance(field, models.OneToOneField)
                 if not rel_is_hidden or o2o:
                     try:
-                        delattr(to, field.related.get_accessor_name())
+                        delattr(remote_field_model, get_remote_field_accessor_name(field))
                     except AttributeError:
                         # Hidden related names are not respected for o2o
                         # thus a tenant models with a o2o pointing to
@@ -108,44 +98,3 @@ def receivers_for_model(model):
 def disconnect_signals(model):
     for signal, receiver in receivers_for_model(model):
         signal.disconnect(receiver, sender=model)
-
-
-if django.VERSION >= (1, 8):
-    def clear_opts_related_cache(model_class):
-        opts = model_class._meta
-        if not opts.apps.ready:
-            return
-        children = [
-            related_object.related_model
-            for related_object in opts.related_objects if related_object.parent_link
-        ]
-        opts._expire_cache()
-        for child in children:
-            clear_opts_related_cache(child)
-else:
-    _opts_related_cache_attrs = [
-        '_related_objects_cache',
-        '_related_objects_proxy_cache',
-        '_related_many_to_many_cache',
-        '_name_map',
-    ]
-
-    def clear_opts_related_cache(model_class):
-        """
-        Clear the specified model and its children opts related cache.
-        """
-        opts = model_class._meta
-        if not opts.apps.ready:
-            return
-        children = [
-            related_object.model
-            for related_object in opts.get_all_related_objects()
-            if related_object.field.rel.parent_link
-        ]
-        for attr in _opts_related_cache_attrs:
-            try:
-                delattr(opts, attr)
-            except AttributeError:
-                pass
-        for child in children:
-            clear_opts_related_cache(child)
