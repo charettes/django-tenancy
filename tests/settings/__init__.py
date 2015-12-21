5a7a3cb66c46fa1fe59795031fd714115b06dff7
Simon Charette <charette.s@gmail.com>
2015-12-20 19:06:47 -0500

    Added a TEMPLATES setting to silence a deprecation warning.

--- tests/settings/__init__.py
+++ tests/settings/__init__.py
@@ -17,3 +17,19 @@ except ImportError:
     pass
 else:
     INSTALLED_APPS.append('mutant')
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
