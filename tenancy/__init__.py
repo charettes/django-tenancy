2ab6d5fa823545c3fb5e138200f73eb48af67b39
Simon Charette <charette.s@gmail.com>
2016-06-08 23:09:26 -0400

    Bumped version number to 0.3.0a4.

--- tenancy/__init__.py
+++ tenancy/__init__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.utils.version import get_version
 
-VERSION = (0, 3, 0, 'alpha', 3)
+VERSION = (0, 3, 0, 'alpha', 4)
 
 __version__ = get_version(VERSION)
 
